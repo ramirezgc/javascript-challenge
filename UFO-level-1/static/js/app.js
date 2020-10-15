450234027a511ba5fa6f9fdba7c074cda1babd0a
Gina Ramirez <67246653+ramirezgc@users.noreply.github.com>
2020-10-14 20:03:50 -0500

    Finish level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -4,14 +4,34 @@ let tableData = data;
 let tbody = d3.select("tbody");
 console.log(tableData);
 
-data.forEach(function(ufoSightings) {
-    console.log(ufoSightings);
-    let row = tbody.append('tr');
-    Object.entries(ufoSightings).forEach(([key, value]) => {
-        console.log(key, value);
-        let cell = row.append('td');
-        cell.text(value);
-    });
-});
+function buildTable(data){
 
-let button = d3.select("#filter-btn");
+    tbody.html("");
+
+    data.forEach(function(ufoSightings) {
+        console.log(ufoSightings);
+        let row = tbody.append('tr');
+        Object.entries(ufoSightings).forEach(([key, value]) => {
+            console.log(key, value);
+            let cell = row.append('td');
+            cell.text(value);
+        });
+    });
+}
+
+function filterTable() {
+
+    const date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+  
+    if (date) {
+      filteredData = filteredData.filter(row => row.datetime === date);
+    }
+    buildTable(filteredData);
+  }
+  
+  d3.selectAll("#filter-btn").on("click", filterTable);
+  
+  buildTable(tableData);
+
+  
